781579c320f2895627d56534dc8202a8619e68a1
Michel du Croix <michelducroix@gmail.com>
2020-03-08 12:10:49 +0100

    campaign cards

--- src/views/Campaigns.vue
+++ src/views/Campaigns.vue
@@ -9,8 +9,14 @@
       </div>
       <div v-else>
         <div v-for="campaign in Campaigns" :key="campaign.name">
-          <div class="campaign">
-            {{ campaign.name }}
+          <div class="campaign" v-bind:class="{ invitedCampaign : campaign.state == 'Invited'}">
+            <div class="CampaignHeader">
+              {{ campaign.name }}
+            </div>
+            <img :src="require('../assets/Scroll.jpg')" class="campaignThumbprint" />
+            <div class="campaignButtons">
+
+            </div>
           </div>
         </div>
       </div>
@@ -51,25 +57,52 @@ export default {
   },
   methods: {
     async fetch() {
-      console.log(JSON.parse(localStorage.User).id);
       await CampaignRepository.getPlayerCampaigns(this.PlayerID).then(response => {
+        response.forEach(campaign => {
+          if (campaign.active == 0 && campaign.dungeonmaster != this.PlayerID) {
+            campaign.state = "Invited";
+          } else if(campaign.dungeonmaster === this.PlayerID) {
+            campaign.state = "DungeonMaster";
+          } else if(campaign.active == 1 && campaign.dungeonmaster != this.PlayerID) {
+            campaign.state = "Active";
+          }
+        });
         this.Campaigns = response;
         this.IsLoaded = true;
       });
-      
     }
   }
 };
 </script>
-<style scoped>
+<style scoped lang="scss">
 .campaign {
     height: 300px;
     width: 200px;
     margin: 10px;
     background-color: rgb(104, 104, 104); 
+    float: left;
+    border-radius: 10px;
+    border: 1px solid;
+    overflow: hidden;  
+}
+
+.invitedCampaign {
+    opacity: 0.5;
 }
 
 .CampaignHeader {
-      background-color: rgb(104, 104, 104);
-    }
+      height: 40px;
+      background-color: rgb(248, 248, 248);    
+      text-align: center;
+      font-weight: bold;
+}
+
+.campaignThumbprint {
+  max-width: 100%;
+  max-height: 100%;
+}
+
+.campaignButtons {
+
+}
 </style>
